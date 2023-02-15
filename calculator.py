02ac76a49ffcfd2abe3a9fbbd14f1ec574b28cef
TP1 <maia.jouenne@gmail.com>
2023-02-15 20:51:34 +0100

    Add division function

--- calculator.py
+++ calculator.py
@@ -6,3 +6,8 @@ if choice == 1:
     a = int(input("Enter the first number: "))
     b = int(input("Enter the second number: "))
     print("Result:", sum(a, b))
+    
+elif choice == 2:
+    a = int(input("Enter the numerator: "))
+    b = int(input("Enter the denominator: "))
+    print("Result:", realDivi(a, b))
